4b03a0c52f81a197485eadd0e96e8ad230bb1cbc
Mustafa Kuscu <mustafakuscu@gmail.com>
2018-09-23 23:24:36 +0300

    some updates for gatsby v2

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,40 +9,20 @@ const {kebabCase, uniq, get, compact, times} = require('lodash');
 const POSTS_PER_PAGE = 10;
 const cleanArray = arr => compact(uniq(arr));
 
-// Add Gatsby's extract-graphql Babel plugin (we'll chain it with babel-loader)
-const extractQueryPlugin = path.resolve(
-  __dirname,
-  `node_modules/gatsby/dist/utils/babel-plugin-extract-graphql.js`
-);
-
-// Temporary workaround to ensure Gatsby builds minified, production build of React.
-// https://github.com/fabien0102/gatsby-starter/issues/39#issuecomment-343647558
-exports.modifyWebpackConfig = ({config, stage}) => {
-  if (stage === 'build-javascript') {
-    config.loader('typescript', {
-      test: /\.tsx?$/,
-      loaders: [
-        `babel-loader?${JSON.stringify({presets: ['babel-preset-env'], plugins: [extractQueryPlugin]})}`,
-        'ts-loader'
-      ]
-    });
-  }
-};
-
 // Create slugs for files.
 // Slug will used for blog page path.
 exports.onCreateNode = ({node, boundActionCreators, getNode}) => {
   const {createNodeField} = boundActionCreators;
   let slug;
   switch (node.internal.type) {
-    case `MarkdownRemark`:
+    case 'MarkdownRemark':
       const fileNode = getNode(node.parent);
       const [basePath, name] = fileNode.relativePath.split('/');
       slug = `/${basePath}/${name}/`;
       break;
   }
   if (slug) {
-    createNodeField({node, name: `slug`, value: slug});
+    createNodeField({node, name: 'slug', value: slug});
   }
 };
 
@@ -57,7 +37,7 @@ exports.createPages = ({graphql, boundActionCreators}) => {
     const templates = ['blogPost', 'tagsPage', 'blogPage']
       .reduce((mem, templateName) => {
         return Object.assign({}, mem,
-        {[templateName]: path.resolve(`src/templates/${kebabCase(templateName)}.tsx`)});
+          {[templateName]: path.resolve(`src/templates/${kebabCase(templateName)}.tsx`)});
       }, {});
 
     graphql(
